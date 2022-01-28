f09bade28437ebaa8e2ae8b70e9b8d39c063cde2
hakasekorin <aegudes@gmail.com>
2022-01-27 18:00:13 -0600

    Adds IUserDao & UserDao. Adds Log4j to gradle
    main includes method to test IUserDao
    refactor this into unit test later

--- project0-template/src/main/java/com/revature/BankManagementDriver.java
+++ project0-template/src/main/java/com/revature/BankManagementDriver.java
@@ -1,8 +1,18 @@
 package com.revature;
 
+import com.revature.dao.UserDao;
+import com.revature.models.Customer;
+import com.revature.service.CustomerService;
+
 public class BankManagementDriver {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
+        CustomerService customerService = new CustomerService();
+        Customer customer = customerService.createNewCustomer("JaneDoe@email.com", "password123");
+
+        UserDao userDao = new UserDao();
+        userDao.createUser(customer);
     }
+
 }
---
+++ project0-template/src/main/java/com/revature/util/LoggerUtil.java
@@ -0,0 +1,7 @@
+package com.revature.util;
+
+public class LoggerUtil {
+
+    private static final String FILE = "log.txt";
+    private static StringBuffer sb;
+}
